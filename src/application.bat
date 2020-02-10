b7675d57963fb18e81782e696b0ba2d9e4adec48
AlexandreBreaultdev <a.breault@iia-laval.fr>
2020-02-10 10:39:32 +0100

    display copyright

--- src/application.bat
+++ src/application.bat
@@ -1,3 +1,4 @@
- ### application.bat - 201x - ALEXANDRE BREAULT ###
+### application.bat - 201x - ALEXANDRE BREAULT ###
+echo "Copyright company - 201x"
 echo "Build : 10:00"
-echo "version 1"
+echo "version 1.1"
